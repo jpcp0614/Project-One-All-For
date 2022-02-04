1bf03567906df38731e3e2bcf6acc5a2079bfe0b
jpcp0614 <jpcp0614@gmail.com>
2022-02-03 22:41:12 -0300

    Desafio 3 - OK

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+SELECT
+	U.`user` `usuario`,
+    COUNT(RH.song_id) `qtde_musicas_ouvidas`,
+    ROUND(SUM(S.duration_seconds)/60, 2) `total_minutos`
+    FROM SpotifyClone.reproduction_history RH
+		INNER JOIN SpotifyClone.`user` U
+			ON U.user_id = RH.user_id
+		INNER JOIN SpotifyClone.song S
+			ON S.song_id = RH.song_id
+		GROUP BY U.user_id
+        ORDER BY `user`;

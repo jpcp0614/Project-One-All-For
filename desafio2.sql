655c7c30d04e9a110aaf8c261cc07039ced8dd1c
jpcp0614 <jpcp0614@gmail.com>
2022-02-03 23:00:02 -0300

    Corrige indentacao

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 SELECT
 	COUNT(S.song_id) `cancoes`,
-    COUNT(DISTINCT(A.artist_id)) `artistas`,
-    COUNT(DISTINCT(A.album)) `albuns`
-FROM SpotifyClone.album A
-	INNER JOIN SpotifyClone.song S
-		ON S.album_id = A.album_id;
+  COUNT(DISTINCT(A.artist_id)) `artistas`,
+  COUNT(DISTINCT(A.album)) `albuns`
+	FROM SpotifyClone.album A
+		INNER JOIN SpotifyClone.song S
+			ON S.album_id = A.album_id;
--- desafio3.sql
+++ desafio3.sql
@@ -3,9 +3,9 @@ SELECT
     COUNT(RH.song_id) `qtde_musicas_ouvidas`,
     ROUND(SUM(S.duration_seconds)/60, 2) `total_minutos`
     FROM SpotifyClone.reproduction_history RH
-		INNER JOIN SpotifyClone.`user` U
-			ON U.user_id = RH.user_id
-		INNER JOIN SpotifyClone.song S
-			ON S.song_id = RH.song_id
-		GROUP BY U.user_id
+			INNER JOIN SpotifyClone.`user` U
+				ON U.user_id = RH.user_id
+			INNER JOIN SpotifyClone.song S
+				ON S.song_id = RH.song_id
+			GROUP BY U.user_id
         ORDER BY `user`;
